4cd25e5e8e28d6fa22a4e77e251b9fa959c105ee
aungkyawkyawspectrum <39019759+aungkyawkyawspectrum@users.noreply.github.com>
2024-07-31 15:33:56 +0630

    css

---
+++ ye-webdesign/JS/Day2-js/operators.js
@@ -0,0 +1,22 @@
+// let age1 = 10;
+// let age2 = 3;
+// // age1++;
+// // age1--;
+
+// // age1 += 10;
+// // age1 += 2;
+// age1 **= 2;
+// console.log(age1);
+
+// let result = "My name is ";
+// result += "John";
+// console.log(result);
+
+// let age1 = 10;
+// let age2 = 2;
+
+// console.log(10 <= 10);
+
+let age = 10;
+let result = age >= 18 ? "This is Adult" : "This is Child";
+console.log(result);
---
+++ ye-webdesign/JS/Day2-js/script.js
@@ -0,0 +1,34 @@
+// let num = 5;
+// console.log(typeof num);
+
+// let username = "John";
+// let fruit = "apple" + " " + username;
+// let stringTemplate = `hello ${username} ${fruit}`;
+
+// console.log(stringTemplate);
+
+// let num = 4 < 6;
+// console.log(num);
+
+// let username;
+// let age = null;
+// let result = username === age;
+// console.log(result);
+
+// let num = "10";
+// let age = 10;
+// console.log(num === age);
+
+// let fruits = ["apple", "orange", "mango"];
+// console.log(fruits[2]);
+
+// const person = {
+//   name: "John",
+//   age: 30,
+// };
+
+// console.log(person.name);
+
+// alert("Hello");
+// prompt("Hello Prompt", "JS");
+confirm("Are you a boss?");
