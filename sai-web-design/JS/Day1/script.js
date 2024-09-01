4637b2d6cd35c549a492c91560e70017cfb13882
aungkyawkyawspectrum <90564038+developer-maythinn@users.noreply.github.com>
2024-09-01 15:29:11 +0630

    js

---
+++ sai-web-design/JS/Day1/script.js
@@ -0,0 +1,47 @@
+// var username = "John";
+// username = "Rose";
+// let age = 20;
+// age = 30;
+// const city = "Yangon";
+// // city = "MDY";
+
+// console.log(username, age, city);
+
+// const username = "John",
+//   age = 25;
+// console.log(username, age);
+
+// let fruit = "orange";
+// let myfruit = fruit;
+// let myFruit = "mango";
+// console.log(fruit);
+// console.log(myfruit);
+// console.log(Myfruit);
+
+// var username  = "Rose";
+
+// if (true) {
+//   var username = "John";
+//   console.log("username", username);
+// }
+// console.log(username);
+
+// "use strict";
+// let num = 5;
+// console.log(num);
+
+// **************************************
+// Data Types
+
+// let num = 5;
+// console.log(typeof num);
+// console.log("not a number" + 2);
+// console.log(1 / 0);
+
+// String
+let username = "John";
+let fruit = "Apple";
+let result = username + " " + fruit + "!";
+let stringTemplate = `${username} ${fruit}!`;
+// console.log(typeof fruit);
+console.log(typeof stringTemplate, stringTemplate);
