1b8109a92ab2ecf264b2c69665d8010c3e527b2d
aungkyawkyawspectrum <90564038+developer-maythinn@users.noreply.github.com>
2024-09-08 12:30:06 +0630

    react

---
+++ ye-webdesign/ES6/Day2/index.js
@@ -0,0 +1,22 @@
+let products = document.querySelector(".products");
+
+function renderProducts(getProducts) {
+  products.innerHTML = getProducts
+    .map((product) => `<p>${product.title}</p>`)
+    .join("");
+}
+async function fetchProducts() {
+  try {
+    const apiResponse = await fetch("https://dummyjson.com/products", {
+      method: "GET",
+    });
+    const result = await apiResponse.json();
+    console.log(result);
+    if (result?.products?.length > 0) {
+      renderProducts(result?.products);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+fetchProducts();
