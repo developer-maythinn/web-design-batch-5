5b90a717f58c4b91d0ec29f3af6855d6fa7f30da
maythinn <90564038+developer-maythinn@users.noreply.github.com>
2024-09-22 18:03:00 +0630

    js

---
+++ kyaw-web-design/JS/Day1/dataTypes.js
@@ -0,0 +1,12 @@
+// number
+// let age = 30;
+// console.log(typeof age);
+
+//string
+let username = "John";
+let stringTemplate = `Hello ${username}. How are you?`;
+let result = "Hello" + " " + username + ". How are you?";
+
+console.log(username, typeof username);
+console.log(stringTemplate);
+console.log(result);
---
+++ kyaw-web-design/JS/Day1/script.js
@@ -0,0 +1,31 @@
+// alert("Hello JS");
+
+// var username = "John";
+// username = "Rose";
+
+// let age = 20;
+// age = 30;
+
+// const fruit = "apple";
+// // fruit = "orange"; // type error
+
+// console.log(username, age, fruit);
+
+// let message = "Hello, How are you?";
+// let greet = message;
+// let Greet = "hello";
+
+// console.log(message);
+// console.log(greet);
+
+var fruit = "orange";
+
+if (true) {
+  let fruit = "apple";
+  console.log(fruit);
+}
+console.log(fruit);
+
+"use strict";
+let num = 5;
+console.log(num);
